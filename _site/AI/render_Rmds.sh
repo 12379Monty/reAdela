684866f7b80b692a2aa4cd9a6e1df809aa4547af
12379Monty <francois.collin@gmail.com>
2025-12-30 18:37:18 -0800

    minor

--- _site/AI/render_Rmds.sh
+++ _site/AI/render_Rmds.sh
@@ -1,11 +1,14 @@
-
+ # Adela_vs_GRAIL
 Rscript -e "rmarkdown::render('_Adela_vs_GRAIL_MCED_Comparison.Rmd')" > _Adela_vs_GRAIL_MCED_Comparison.log 
 
+ # cfMeDIP-seq
 Rscript -e "rmarkdown::render('_cfMeDIP-seq_Protocol_Pipeline_Classifiers.Rmd')" > _cfMeDIP-seq_Protocol_Pipeline_Classifiers.log  
 
+ # DMA
 Rscript -e "rmarkdown::render('_Differential_Methylation_Analysis_Report.Rmd')" > _Differential_Methylation_Analysis_Report.log
 
 
+ # Enrichment_Methods
 Rscript -e "rmarkdown::render('_Enrichment_Methods_PartI_Section1.Rmd')" > _Enrichment_Methods_PartI_Section1.log  
 
 
